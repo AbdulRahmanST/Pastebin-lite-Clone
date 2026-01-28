36a3755fa30c529e4b621fe975b0747855f7ad6e
Abdul Rahman ST <starahman006@gmail.com>
2026-01-28 23:43:09 +0530

    Serve root route from Express

--- src/server.js
+++ src/server.js
@@ -5,6 +5,10 @@ import pastes from "./routes/pastes.js";
 const app = express();
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Pastebin Lite API Running");
+});
+
 app.use("/api/healthz", health);
 app.use("/api/pastes", pastes);
 app.use("/p", pastes);
